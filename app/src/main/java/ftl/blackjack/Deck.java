5d25d1f8d0f678e3a03fb4fcde2d5caf3343eebf
Yuen Hsi <yuenhsi93@gmail.com>
2016-11-06 16:41:45 -0800

    add Deck class, implement draw method; reshuffle concept pending.

---
+++ app/src/main/java/ftl/blackjack/Deck.java
@@ -0,0 +1,39 @@
+package ftl.blackjack;
+
+// concept of reshuffling and adding all cards once a threshold is hit needs to be implemented. This may be done with a static reshuffle variable.
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+
+    private static int totalCards;
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                Card card = new Card(suit, rank);
+                deck.add(card);
+                totalCards++;
+            }
+        }
+    }
+
+    public Card draw() {
+        Random random = new Random();
+        int n = random.nextInt(deck.size());
+        Card drawnCard = deck.get(n);
+        deck.set(n, deck.get(deck.size() - 1)); // copy the card on the bottom of the deck with the drawn card
+        deck.remove(deck.size() - 1);
+        return drawnCard;
+    }
+
+    public ArrayList getDeck() {
+        return deck;
+    }
+
+    public static int getTotalCards() {
+        return totalCards;
+    }
+}
