3d23d167fe3c00406e93ee26b79875dc15def9f5
Yuen Hsi <yuenhsi93@gmail.com>
2016-11-11 16:24:39 -0800

    add relevant game files

--- app/src/main/java/ftl/blackjack/BlackJackGameActivity.java
+++ app/src/main/java/ftl/blackjack/BlackJackGameActivity.java
@@ -1,5 +1,13 @@
 package ftl.blackjack;
 
-public class BlackJackGameActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class BlackJackGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_jack_game);
+    }
 }
